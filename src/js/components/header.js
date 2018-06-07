3b278e697d1b0e0b667a38fad81fc6e11381031c
Alex Viki <shevashokc@gmail.com>
2018-06-07 19:41:33 +0300

    Was finished sterilisation "burger-menu"

--- src/js/components/header.js
+++ src/js/components/header.js
@@ -2,8 +2,6 @@ var btn = $('.header-body__burger');
 var menu = $('.header-body');
 var mask = $('.mask');
 
-//console.log(btn);
-
 function menuShow() {
     btn.on('click', function() {
         menu.toggleClass('is-active');       mask.toggleClass('is-active');
