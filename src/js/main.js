01ffadf340fc006097989cc4410b93efc3f52063
Alex Viki <shevashokc@gmail.com>
2018-06-13 08:53:19 +0300

    Added slider to block <!-- Begins Features -->

--- src/js/main.js
+++ src/js/main.js
@@ -5,11 +5,23 @@ console.log('Hi from main.js');
 // функция описана в файле header.js
 $(window).ready(function() {
     menuShow();
+
+    // запуск слайдера с ресурса flickity.metafizzy.co
+    // для блока <!-- Begins Meet Brandi! -->
+    $('.intro-list').flickity({
+        cellAlign: 'left',
+        contain: true,
+        prevNextButtons: false
+    });
+
+    // запуск слайдера с ресурса flickity.metafizzy.co
+    // для блока <!-- Begins Meet Brandi! -->
+    $('.features-items').flickity({
+        cellAlign: 'left',
+        contain: true,
+        prevNextButtons: false
+    });
+
+
 })
 
-// запуск слайдера с ресурса flickity.metafizzy.co
-$('.intro-list').flickity({
-    cellAlign: 'left',
-    contain: true,
-    prevNextButtons: false
-});
